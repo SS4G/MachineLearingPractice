a0367f538adca06359e416c4a84621699f8e52c3
jumper_g5501 <ziheng_song@126.com>
2017-10-19 22:24:39 +0800

    2nd

---
+++ show.py
@@ -0,0 +1,9 @@
+print("cfg")
+with open("dir_cfg.ini") as f:
+    lines = f.readlines()
+    dictx = {}
+    dictx["name"] = lines[0].split('=')[0].strip()
+    dictx["srcDir"] = lines[1].split('=')[0].strip()
+    dictx["dataDir"] = lines[1].split('=')[0].strip()
+    for k in dictx:
+        print(k, dictx[k])
